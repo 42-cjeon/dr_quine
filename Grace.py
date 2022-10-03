1f461f97b2bd21755611345f84e21da560185018
cjeon <cjeon@student.42seoul.kr>
2022-10-03 13:24:03 +0900

    feat: Impl python version of Grace

--- Grace.py
+++ Grace.py
@@ -1,5 +1,13 @@
-def decl_main(body):
-  exec(f'  def main():\n  {body}')
-  return main
+NL = chr(10)
+DQ = chr(34)
 
-decl_main('print("hello")')()
+s = "NL = chr(10)%cDQ = chr(34)%c%cs = %c%s%c%c%cdef decl_main(body):%c  exec(f'def main():{NL}  {body}', globals())%c%cdecl_main(%cprint(s %% (NL, NL, NL, DQ, s, DQ, NL, NL, NL, NL, NL, DQ, DQ, NL, NL, NL, NL), end='')%c)%c%c# ANYWAY IT WORKS!%cif __name__ == '__main__':%c  main()"
+
+def decl_main(body):
+  exec(f'def main():{NL}  {body}', globals())
+
+decl_main("print(s % (NL, NL, NL, DQ, s, DQ, NL, NL, NL, NL, NL, DQ, DQ, NL, NL, NL, NL), end='')")
+
+# ANYWAY IT WORKS!
+if __name__ == '__main__':
+  main()
